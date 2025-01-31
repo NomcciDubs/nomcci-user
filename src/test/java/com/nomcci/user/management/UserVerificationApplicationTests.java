10bb7d7ecb5cf538fb6c021989987f74ad503355
Andres Garcia <91227751+NomcciDubs@users.noreply.github.com>
2025-01-31 17:26:27 -0500

    v0.2.5 Uso de AWS

--- src/test/java/com/nomcci/user/management/UserVerificationApplicationTests.java
+++ src/test/java/com/nomcci/user/management/UserVerificationApplicationTests.java
@@ -3,7 +3,7 @@ package com.nomcci.user.management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=aws"})
 class UserVerificationApplicationTests {
 
     @Test
